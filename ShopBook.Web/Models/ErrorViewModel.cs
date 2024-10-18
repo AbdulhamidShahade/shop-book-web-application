06d82ccaafaae2d63c6a9bec547a8185a21c4df4
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-10-18 21:54:40 +0300

    Add project files.

---
+++ ShopBook.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ShopBook.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
