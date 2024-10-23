2c47ac5b43f1d3b9c0cf7e909cb2abcc7a0654c4
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-10-23 23:44:48 +0300

    Add Product abstract service

---
+++ ShopBook.Web/Services/ProductService.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using ShopBook.Web.Models;
+
+namespace ShopBook.Web.Services
+{
+    public class ProductService : IProductService
+    {
+        private HttpClient _httpClient;
+        private string _url;
+
+        public ProductService(HttpClient httpClient, IConfiguration configuration)
+        {
+            _httpClient = httpClient;
+            _url = configuration.GetValue<string>("ApiUrls:ProductsUrl");
+        }
+
+        public async Task<ApiResponse> GetProductsAsync(string point)
+        {
+            var response = await _httpClient.GetStringAsync($"{_url}/{point}");
+
+            ApiResponse apiResponse = JsonConvert.DeserializeObject<ApiResponse>(Convert.ToString(response));
+
+            return apiResponse;
+        }
+    }
+}
