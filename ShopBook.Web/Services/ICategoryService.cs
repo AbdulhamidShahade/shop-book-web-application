fba7d8c137f2dd39659aa9810012406b3eb70fc7
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-10-23 23:43:55 +0300

    Add ICategory abstract service

---
+++ ShopBook.Web/Services/ICategoryService.cs
@@ -0,0 +1,11 @@
+﻿
+
+using ShopBook.Web.Models;
+
+namespace ShopBook.Web.Services
+{
+    public interface ICategoryService
+    {
+        Task<IEnumerable<Category>> GetProductCategoriesAsync();
+    }
+}
