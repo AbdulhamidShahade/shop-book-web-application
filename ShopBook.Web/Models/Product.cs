d6c730296633b29a3ed526980eec56e272ca7438
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-10-23 23:45:39 +0300

    Update product model

--- ShopBook.Web/Models/Product.cs
+++ ShopBook.Web/Models/Product.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
-        public string Category { get; set; }
+        public Category Category { get; set; }
         public double Price { get; set; }
         public double DiscountPercentage { get; set; }
         public double Rating { get; set; }
